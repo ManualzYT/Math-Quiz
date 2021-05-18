bf35d60ba9c4921eaf8cce0abb61c770be66e70f
ManualzYT <75396652+ManualzYT@users.noreply.github.com>
2021-05-17 17:53:57 -0700

    Add files via upload

--- quiz.js
+++ quiz.js
@@ -5,8 +5,8 @@
 	player1_score = 0;
 	player2_score = 0;
 
-document.getElementById("player1_name").innerHTML = player1_name + " : ";
-document.getElementById("player2_name").innerHTML = player2_name + " : ";
+document.getElementById("player1_name").innerHTML = player1_name + ":";
+document.getElementById("player2_name").innerHTML = player2_name + ":";
 
 document.getElementById("player1_score").innerHTML = player1_score ;
 document.getElementById("player2_score").innerHTML = player2_score ;
@@ -43,11 +43,13 @@ function check()
 		{
 			player1_score = player1_score +1;
 		    document.getElementById("player1_score").innerHTML = player1_score;
+			document.getElementById('2ndgame_status').innerHTML = "Nice job!";
 		}
 		else 
 		{
 			player2_score = player2_score +1;
 		    document.getElementById("player2_score").innerHTML = player2_score;
+			document.getElementById('2ndgame_status').innerHTML = "Nice job!";
 		}
 	}
 	
@@ -55,22 +57,27 @@ function check()
 	{
 		question_turn = "player2"
 		document.getElementById("player_question").innerHTML = "Question Turn - " + player2_name ;
+		
 	}
 	else 
 	{
 		question_turn = "player1"
 		document.getElementById("player_question").innerHTML = "Question Turn - " + player1_name ;
+		
 	}
 
 	if(answer_turn == "player1")
 	{
 		answer_turn = "player2"
 		document.getElementById("player_answer").innerHTML = "Answer Turn - " + player2_name ;
+		document.getElementById('game_status').innerHTML = "Wrong answer try another question  : )";
+		
 	}
 	else 
 	{
 		answer_turn = "player1"
-		document.getElementById("player_answer").innerHTML = "Answer Turn - " + player1_name ;
+		document.getElementById("player_answer").innerHTML = "Answer Turn - " + player1_name;
+		
 	}
 
     document.getElementById("output").innerHTML = "";
