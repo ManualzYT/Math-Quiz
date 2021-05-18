1fc7b7c52417edd0609a2e779fcd5f3e1643878b
ManualzYT <75396652+ManualzYT@users.noreply.github.com>
2021-05-17 20:09:30 -0700

    Add files via upload

--- quiz_login.js
+++ quiz_login.js
@@ -1,10 +1,10 @@
-function addUser();
+function addUser()
 {
-player1_name = document.getElementById("player1_name_input").value;
-player2_name = document.getElementById("player2_name_input").value;
-localStorage.setItem("player1_name" , player1_name);
-localStorage.setItem("player2_name" , player2_name);
-window.location.href = "http://127.0.0.1:5500/quiz.html";    
+
+    player1_name = document.getElementById("player1").value;
+    player2_name = document.getElementById("player2").value;
+
+    localStorage.setItem("player1_name" , player1_name);
+    localStorage.setItem("player2_name" , player2_name);
+    window.location = "quiz.html";
 }
-
-
